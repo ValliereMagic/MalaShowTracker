9ad48367ddb05ec7d695b5a989657c3d6025f9ef
FRadford <francis.radford@outlook.com>
2018-03-14 23:02:44 -0400

    Added docstrings and type hints

--- src/Main.py
+++ src/Main.py
@@ -1,39 +1,41 @@
-from copy import deepcopy
-import sys
 import os
-import sqlite3
 import pickle
-from ShowList import ShowList
+import sqlite3
+import sys
+import typing
+from copy import deepcopy
+
 import CLI
+import Show
+import ShowList
 
 
-# initialize database, and return connection
-
-
-def init_database():
-    db_filename = "../data/shows.db"
-    db_is_new = not os.path.exists(db_filename)
+def init_database() -> sqlite3.Connection:
+    """
+    Initialize database and return connection
+    """
+    db_filename: str = "../data/shows.db"
+    db_is_new: bool = not os.path.exists(db_filename)
 
     if db_is_new:
         with sqlite3.connect(db_filename) as connection:
             with open("../data/ShowTable.sql", 'rt') as f:
-                schema = f.read()
+                schema: str = f.read()
                 connection.execute(schema)
                 return connection
 
     return sqlite3.connect(db_filename)
 
 
-# return show_list stored in the database if it exists.
-# otherwise return None.
-
-
-def get_show_list(connection):
-    cursor = connection.cursor()
+def get_show_list(connection: sqlite3.Connection) -> ShowList.ShowList or None:
+    """
+    Return show_list stored in database if it exists, otherwise return None
+    """
+    cursor: sqlite3.Cursor = connection.cursor()
 
     cursor.execute("select * from ShowTable where id = 1")
 
-    retrieved_data = cursor.fetchall()
+    retrieved_data: typing.List[sqlite3.Row] = cursor.fetchall()
 
     if not len(retrieved_data) == 0:
         for row in retrieved_data:
@@ -43,17 +45,17 @@ def get_show_list(connection):
     return None
 
 
-# save the show_list object passed, into the database
-
-
-def save_show_list(connection, show_list):
-    pickled_show_list = pickle.dumps(show_list)
-    cursor = connection.cursor()
+def save_show_list(connection: sqlite3.Connection, show_list: ShowList.ShowList):
+    """
+    Save the show_list object given into the database
+    """
+    pickled_show_list: bytes = pickle.dumps(show_list)
+    cursor: sqlite3.Cursor = connection.cursor()
 
     cursor.execute("select * from ShowTable")
-    record_exists = cursor.fetchall()
+    record_exists: typing.List[sqlite3.Row] = cursor.fetchall()
 
-    query = "update ShowTable set Show_List = ? where id = 1"
+    query: str = "update ShowTable set Show_List = ? where id = 1"
 
     if len(record_exists) == 0:
         query = "insert into ShowTable (Show_List) values (?)"
@@ -63,35 +65,38 @@ def save_show_list(connection, show_list):
 
 
 def main():
-    connection = init_database()
+    """
+    Handles input from user and calls appropriate functions
+    """
+    connection: sqlite3.Connection = init_database()
 
-    show_list = get_show_list(connection)
+    show_list: ShowList.ShowList or None = get_show_list(connection)
 
     # check to see if the list has been modified, and needs to be saved.
-    modify_check_list = deepcopy(show_list)
+    modify_check_list: ShowList.ShowList or None = deepcopy(show_list)
 
     if show_list is None:
-        show_list = ShowList()
+        show_list: ShowList.ShowList = ShowList.ShowList()
 
     # remove the name of the script from arguments
-    args = sys.argv
+    args: list = sys.argv
     args.remove(sys.argv[0])
 
-    entered_arg_loop = False
+    entered_arg_loop: bool = False
 
     for arg in args:
         entered_arg_loop = True
-        arg = arg.lower()
+        arg: str = arg.lower()
 
         if arg == "add_show":
             show_list.add_show()
 
         elif arg == "remove_show":
-            confirmation = show_list.remove_show(CLI.request_show_name())
+            confirmation: bool = show_list.remove_show(CLI.request_show_name())
             CLI.action_confirmation(confirmation, "show removal")
 
         elif arg == "get_show":
-            requested_show_with_index = show_list.find_show(CLI.request_show_name())
+            requested_show_with_index: Show.ShowWithIndex or None = show_list.find_show(CLI.request_show_name())
 
             if requested_show_with_index is not None:
                 CLI.pretty_print_show(requested_show_with_index.show)
@@ -104,11 +109,11 @@ def main():
                 CLI.pretty_print_show(show)
 
         elif arg == "increment_show_episode":
-            confirmation = show_list.increment_show(CLI.request_show_name())
+            confirmation: bool = show_list.increment_show(CLI.request_show_name())
             CLI.action_confirmation(confirmation, "incrementing episode")
 
         elif arg == "increment_show_season":
-            confirmation = show_list.increment_show_season(CLI.request_show_name())
+            confirmation: bool = show_list.increment_show_season(CLI.request_show_name())
             CLI.action_confirmation(confirmation, "incrementing season")
 
         else:
--- src/ShowList.py
+++ src/ShowList.py
@@ -1,24 +1,26 @@
+import typing
+
+import Show
 from CLI import request_new_show
-from Show import ShowWithIndex
 
 
 class ShowList:
     def __init__(self):
-        # shows : Show Objects
-        self.shows = []
-
-    # Get show object, add to shows list
+        self.shows: typing.List[Show.Show] = []
 
     def add_show(self):
-        new_show = request_new_show()
+        """
+        Get show object, add to shows list
+        """
+        new_show: Show.Show = request_new_show()
         self.shows.append(new_show)
 
-    # takes a list of show objects, and the name of a specific show, then removes the show
-    # with the corresponding name if it is found.
-    # If a show with that name is not found, then the method returns None.
-
-    def remove_show(self, show_name):
-        show_with_index = self.find_show(show_name)
+    def remove_show(self, show_name: str) -> bool:
+        """
+        Takes the name of a specific show and removes the show with the corresponding name from the instance's show
+        list. Returns True if the show is found, False otherwise
+        """
+        show_with_index: Show.ShowWithIndex = self.find_show(show_name)
 
         if show_with_index is not None:
             self.shows.remove(show_with_index.show)
@@ -26,11 +28,12 @@ class ShowList:
 
         return False
 
-    # takes the show list of Show objects and a show name, and increments the episodes_watched
-    # property of the object by one, and then, returns the updated list.
-
-    def increment_show(self, show_name):
-        show_with_index = self.find_show(show_name)
+    def increment_show(self, show_name: str) -> bool:
+        """
+        Takes a show name, increments its episodes_watched property and updates the instance's show list. Returns
+        True if the show is found, False otherwise
+        """
+        show_with_index: Show.ShowWithIndex = self.find_show(show_name)
 
         if show_with_index is not None:
             show_with_index.show.episodes_watched += 1
@@ -39,11 +42,12 @@ class ShowList:
 
         return False
 
-    # get show by name, increment season number, and set
-    # episodes_watched to 0.
-
-    def increment_show_season(self, show_name):
-        show_with_index = self.find_show(show_name)
+    def increment_show_season(self, show_name: str) -> bool:
+        """
+        Takes a show name, increments its season_number property, resets episodes_watched to 0, and updates the
+        instance's show list. Returns True if the show is found, False otherwise
+        """
+        show_with_index: Show.ShowWithIndex = self.find_show(show_name)
 
         if show_with_index is not None:
             show_with_index.show.season_number += 1
@@ -53,10 +57,12 @@ class ShowList:
 
         return False
 
-    # set the value of the current episode count
-
-    def set_show_episodes(self, show_name, episode_value):
-        show_with_index = self.find_show(show_name)
+    def set_show_episodes(self, show_name: str, episode_value: int) -> bool:
+        """
+        Takes a show name and episode value, sets the episodes_watched property and updates the instance's show list.
+        Returns True if the show is found, False otherwise
+        """
+        show_with_index: Show.ShowWithIndex = self.find_show(show_name)
 
         if show_with_index is not None:
             show_with_index.show.episodes_watched = episode_value
@@ -65,10 +71,12 @@ class ShowList:
 
         return False
 
-    # set the value of the current season number (int)
-
-    def set_show_seasons(self, show_name, season_value):
-        show_with_index = self.find_show(show_name)
+    def set_show_seasons(self, show_name: str, season_value: int) -> bool:
+        """
+        Takes a show name and season value, sets the season_number property and updates the instance's show list.
+        Returns True if the show is found and False otherwise
+        """
+        show_with_index: Show.ShowWithIndex = self.find_show(show_name)
 
         if show_with_index is not None:
             show_with_index.show.season_number = season_value
@@ -77,15 +85,16 @@ class ShowList:
 
         return False
 
-    # takes a list of Show objects and the (string) name of a show, and looks through the list passed
-    # to find the object with the show name. Assuming that show names are unique.
-
-    def find_show(self, show_name):
-        temp_show_object = None
+    def find_show(self, show_name: str) -> Show.ShowWithIndex:
+        """
+        Takes a show name and looks through the instance's show list for a matching name, assuming that show names
+        are unique.
+        """
+        temp_show_object: Show.ShowWithIndex = None
 
         for index, show in enumerate(self.shows):
             if show.name.lower() == show_name.lower():
-                temp_show_object = ShowWithIndex(show, index)
+                temp_show_object = Show.ShowWithIndex(show, index)
                 break
 
         return temp_show_object
